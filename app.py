65b4aa233b64ac32212d331aa4d287385b404a43
Manish Manandhar <mdhrmanish7@gmail.com>
2026-09-07 22:50:50 +0545

    added new workflow

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
 import requests
-import json
-response = requests.get("http://api.open-notify.org/astros.json")
 
-output = response.json()
+def main():
+    r = requests.get("https://catfact.ninja/fact")
+    print(" Cat Fact: ", r.json()['fact'])
 
-pretty_json = json.dumps(output, indent=5)
-
-list_of_people = output.get("people")
-
-def list_people():
-    print(f"the people current in space are:")
-    for people in list_of_people:
-        print(people.get("name"))
-
-list_people()
+if __name__ == "__main__":
+    main()
