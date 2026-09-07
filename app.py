c80bad3e8c9b0979ea363f1b441ca7b542121916
Manish Manandhar <mdhrmanish7@gmail.com>
2026-09-07 22:16:29 +0545

    added app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("Hello World")
+import requests
+import json
+response = requests.get("http://api.open-notify.org/astros.json")
+
+#output = response.json()
+
+pretty_json = json.dumps(output, indent=5)
+
+list_of_people = output.get("people")
+
+def list_people():
+    print(f"the people current in space are:")
+    for people in list_of_people:
+        print(people.get("name"))
+
+list_people()
